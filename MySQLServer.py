9f4bcd5d62e4ca9a5e0e2149f4da7e960f321df2
Mohamed Hossam <hamo08521hamo@gmail.com>
2025-06-29 22:30:48 +0300

    Add clean MYSQL connector script to create alx_book_store

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,8 +1,6 @@
 import mysql.connector
-from mysql.connector import Error
 
 try:
-    
     connection = mysql.connector.connect(
         host='localhost',
         user='root',
@@ -14,10 +12,10 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
-    print(f"Error: {e}")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
 
 finally:
-    if connection.is_connected():
+    if 'connection' in locals() and connection.is_connected():
         cursor.close()
         connection.close()
